ed432f8dde752baa2f32f505f09ddcda01af0f6d
superkookai <weerawut@live.com>
2025-12-09 17:37:30 +0700

    Initial Commit

---
+++ SupabaseMarketPlace/SupabaseMarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseMarketPlaceApp.swift
+//  SupabaseMarketPlace
+//
+//  Created by Weerawut Chaiyasomboon on 09/12/2568.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseMarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
